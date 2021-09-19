b2acfc41d1d2ceb68a9aada0bc3f01413d1dcd34
root <root@localhost>
2021-09-19 11:58:27 +0200

    created menu

--- game.py
+++ game.py
@@ -8,24 +8,77 @@ from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 
 
+optionbars = []
+selectbars = []
+keypressed = False
 class Gamestate(StateMachine):
-    menu = State('Menu', initial=True)
+
+    init = State('init', initial= True)
+    menu = State('Menu')
     game = State('Game')
 
+    start = init.to(menu)
     play = menu.to(game)
+    def on_play(self):
+        print('starting game')
+    def on_start(self):
+        print('initmenu')
+
+        for i in range(0, 2):
+            selectbars.append(Rect((WIDTH / 2 - 110 + (200 * i), HEIGHT / 3), (20, 50)))
+        for i in range(0, 4):
+            optionbars.append(Rect((WIDTH / 2 - 100, HEIGHT / 3 + (70 * i)), (200, 50)))
+
     def update(self):
+        global keypressed
+        if self.is_init:
+            self.start()
         if self.is_menu:
+            if keyboard.down:
+                if not keypressed:
+                    for selectbar in selectbars:
+                        if not selectbar.colliderect(optionbars[-1]):
+                            if not selectbar.y > optionbars[-1].y:
+                                selectbar.y += 70
+                    keypressed = True
+            elif keyboard.up:
+                if not keypressed:
+                    for selectbar in selectbars:
+                        if not selectbar.colliderect(optionbars[0]):
+                            if not selectbar.y < optionbars[0].y:
+                                selectbar.y -= 70
+                    keypressed = True
+            else:
+                keypressed = False
             if keyboard.space:
-                self.play()
+                if selectbars[0].y == optionbars[0].y:
+                    self.play()
+                if selectbars[0].y == optionbars[1].y:
+                    print('help')
+                if selectbars[0].y == optionbars[2].y:
+                    print('options')
+                elif selectbars[0].y == optionbars[-1].y:
+                    exit()
+    def draw(self):
+        if self.is_menu:
+            screen.clear()
+            #screen.fill('white')
+            for optionbar in optionbars:
+                screen.draw.filled_rect(optionbar, (100, 200, 100))
+            for selectbar in selectbars:
+                screen.draw.filled_rect(selectbar, (150, 250, 50))
+            screen.draw.text('PLAY GAME', (WIDTH / 2 - 110 + 50, HEIGHT / 3 + 15), fontsize = 30)
+            screen.draw.text('HELP', (WIDTH / 2 - 110 + 80, HEIGHT / 3 + 85), fontsize=30)
+            screen.draw.text('OPTIONS', (WIDTH / 2 - 110 + 60, HEIGHT / 3 + 155), fontsize=30)
+            screen.draw.text('EXIT', (WIDTH / 2 - 110 + 80, HEIGHT / 3 + 225), fontsize=30)
+        if self.is_game:
+            screen.clear()
 gmstate = Gamestate()
 
 def update():
     gmstate.update()
-    print(gmstate.current_state)
-
-
 def draw():
-    pass
+    gmstate.draw()
 
 
 pgzrun.go()
